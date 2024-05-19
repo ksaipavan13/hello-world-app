b4bf32b42e6d122c24625d14de44d8c27a444bd9
saipavan <ksp2001961@gmail.com>
2024-05-19 15:47:53 +0530

    Add GitHub Actions workflow for ECS deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,8 +13,7 @@ resource "aws_subnet" "main" {
 }
 
 resource "aws_security_group" "ecs_service" {
-  name_prefix = "ecs-service-"
-  vpc_id      = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 3000
@@ -31,6 +30,30 @@ resource "aws_security_group" "ecs_service" {
   }
 }
 
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecsTaskExecutionRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_cloudwatch_log_group" "log_group" {
+  name              = "ecs-log-group"
+  retention_in_days = 7
+}
+
 resource "aws_ecs_cluster" "hello_world_cluster" {
   name = "hello-world-cluster"
 }
@@ -41,20 +64,25 @@ resource "aws_ecs_task_definition" "hello_world_task" {
   requires_compatibilities = ["FARGATE"]
   cpu                      = "256"
   memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_task_execution_role.arn
 
-  container_definitions = jsonencode([
-    {
-      name      = "hello-world-container"
-      image     = "ksaipavan13/hello-world-app:latest"
-      essential = true
-      portMappings = [
-        {
-          containerPort = 3000
-          hostPort      = 3000
-        }
-      ]
+  container_definitions = jsonencode([{
+    name  = "hello-world-container"
+    image = "ksaipavan13/hello-world-app:latest"
+    essential = true
+    portMappings = [{
+      containerPort = 3000
+      hostPort      = 3000
+    }]
+    logConfiguration = {
+      logDriver = "awslogs"
+      options = {
+        awslogs-group         = "ecs-log-group"
+        awslogs-region        = "us-west-2"
+        awslogs-stream-prefix = "ecs"
+      }
     }
-  ])
+  }])
 }
 
 resource "aws_ecs_service" "hello_world_service" {
@@ -62,12 +90,15 @@ resource "aws_ecs_service" "hello_world_service" {
   cluster         = aws_ecs_cluster.hello_world_cluster.id
   task_definition = aws_ecs_task_definition.hello_world_task.arn
   desired_count   = 1
-  launch_type     = "FARGATE"
 
   network_configuration {
     subnets          = [aws_subnet.main.id]
     security_groups  = [aws_security_group.ecs_service.id]
     assign_public_ip = true
   }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_task_execution_role_policy
+  ]
 }
 
